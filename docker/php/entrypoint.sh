6a3ff8a4f2482f736df2e415484151c92f243589
Oli Raimond <oraimond@umich.edu>
2025-12-24 17:15:22 +1100

    stop using wp migration

--- docker/php/entrypoint.sh
+++ docker/php/entrypoint.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 set -e
 
-# Ensure PHP temp folder exists and is writable
-mkdir -p /var/www/html/tmp
-chown -R www-data:www-data /var/www/html/tmp
-
 # Wait for WordPress to be installed
 # until wp core is-installed --path=/var/www/html --quiet --allow-root; do
 #     echo "Waiting for WordPress to be installed..."
@@ -22,15 +18,6 @@ if [ ! -d "/var/www/html/wp-content/plugins/redis-cache" ]; then
     unzip /tmp/redis.zip -d /var/www/html/wp-content/plugins/
     rm /tmp/redis.zip
     chown -R www-data:www-data /var/www/html/wp-content/plugins/redis-cache
-fi
-
-# Install All-in-One WP Migration plugin if not already installed
-if [ ! -d "/var/www/html/wp-content/plugins/all-in-one-wp-migration" ]; then
-    echo "Installing All-in-One WP Migration plugin..."
-    curl -o /tmp/ai1wm.zip -SL https://downloads.wordpress.org/plugin/all-in-one-wp-migration.latest-stable.zip
-    unzip /tmp/ai1wm.zip -d /var/www/html/wp-content/plugins/
-    rm /tmp/ai1wm.zip
-    chown -R www-data:www-data /var/www/html/wp-content/plugins/all-in-one-wp-migration
 fi
 
 # Activate plugin if not activated
